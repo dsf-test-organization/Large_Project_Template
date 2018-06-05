64248d55cdc3decfc0dad1439bb98cfe2b253d40
Buzzee <benjamin.buzzee@dfg.alaska.local>
2018-06-04 16:10:27 -0800

    document data

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Lengths of 100 imaginary fishes
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#'  diamonds. The variables are as follows:
+#'  Who:
+#'  What:
+#'  Where:
+#'  When:
+#'  Description of or link to sampling protocols:
+#'
+#' @format A data frame with 100 rows and 1 variable:
+#' \describe{
+#'   \item{x}{Lengths of imaginary fish measured in centimeters}
+#' }
+"fish_lengths"
+
+# how to save packaged data
+# fish_lengths <- rnorm(100, mean = 100, sd = 20)
+#
+# devtools::use_data(fish_lengths)
