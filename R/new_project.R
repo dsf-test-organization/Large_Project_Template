fc456d29e4939ade65650edfd5e09cadaac29243
Buzzee <benjamin.buzzee@dfg.alaska.local>
2018-06-07 09:29:46 -0800

    new_proj function

--- R/new_project.R
+++ R/new_project.R
@@ -1,41 +1,42 @@
 #' Create new project scaffolding.
 #'
-#' Create all the scaffolding for a new project in a new directory. To use: 1. Go to https://github.com/settings/tokens to retieve your auth_token that will be used in the function.
-#' 2. Change your working directory to the location where you want the project folder to be stored 3. Run the function.
+#' Creates all the scaffolding for a new project in a new directory. To use: 1. Go to https://github.com/settings/tokens to retrieve your auth_token.
+#' 2. Change your working directory to the location you want the project folder to be stored 3. Run the function. NOTE: folders cannot be added
+#' to github until there is a file in them. The github repo will be created under your personal account. You will need to transfer or fork the repo to the
+#' dsf organization account.
 #'
-#' @param name Name of the new project. A new folder will be in the created current working directory with that name.
-#' @param github Logical. Create GitHub repo? Note this requires a \code{GITHUB_PAT}. See \code{\link[devtools]{use_github}}.
-#' @return A new directory with R package structure, slightly modified.
+#' @param name Name of the new project. A new folder will be in the created current working directory with that name
+#' @param github Logical. Create GitHub repo? Note this requires an auth_token
+#' @return A new directory with a modified R package
 #' @export
 #' @import devtools
-#' @examples  new_project("pkgName", github = TRUE, auth_token = "a12k3ln0812143cla292f2asd81226eb")
+#' @examples new_project("pkgName", github = TRUE, auth_token = "a12k3l812143cla292f2asd81226eb")
 
-new_project <- function(name, github = FALSE, auth_token = NULL){
-  print("dev tools create name")
+new_project <- function(name, github = TRUE, auth_token = "get_from_github"){
+
+
   devtools::create(name)
-  print("trying")
-  try(use_package_doc(name), silent = TRUE)
-  try(use_readme_rmd(name), silent = TRUE)
+  try(devtools::use_package_doc(name), silent = TRUE)
+  try(devtools::use_readme_rmd(name), silent = TRUE)
 
-  print("use stuff")
-  # use_data_raw(name)
-  use_testthat(name)
+
+
+  devtools::use_package("tidyverse", "Suggests")
+
 
 
   dir.create(file.path(name, "data"))
   dir.create(file.path(name, "data-raw"))
   dir.create(file.path(name, "final_analysis"))
   dir.create(file.path(name, "op_plan"))
-  # cat("\n^analyses$ \n^manuscript$ \n", sep = "",
-  #     file = file.path(name, ".Rbuildignore"), append = TRUE)
+
+
 
   if (github){
     use_github(pkg = name, auth_token = auth_token, protocol = "https")
-    use_github_links(name, auth_token = auth_token)
-
 
   }
 
 }
 
-
+ new_project("AutoProj", github = TRUE, auth_token = "84d95ce5dec060ed067a211e79b3305cc9ef4071")
