02df68870838b93230544dbe143322ed275d46de
Buzzee <benjamin.buzzee@dfg.alaska.local>
2018-06-07 10:03:20 -0800

    wording

--- R/new_project.R
+++ R/new_project.R
@@ -1,8 +1,9 @@
 #' Create new project scaffolding.
 #'
-#' Creates all the scaffolding for a new project in a new directory. To use: 1. Go to https://github.com/settings/tokens to retrieve your auth_token.
-#' 2. Change your working directory to the location you want the project folder to be stored 3. Run the function. NOTE: folders cannot be added
-#' to github until there is a file in them. The github repo will be created under your personal account. You will need to transfer or fork the repo to the
+#' Creates all the scaffolding for a new project in a new directory. To use: 1. Go to https://github.com/settings/tokens
+#' to retrieve your auth_token. 2. Change your working directory to the location you want the project folder to be
+#' stored 3. Run the function. NOTE: folders cannot be added to github until there is a file in them.
+#' The github repo will be created under your personal account. You will need to transfer or fork the repo to the
 #' dsf organization account.
 #'
 #' @param name Name of the new project. A new folder will be in the created current working directory with that name
